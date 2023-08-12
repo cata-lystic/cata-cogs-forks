10a66cbf5d44e8b3d5f336f1c190e073c73263ea
cata-lystic <ntqtmslf@protonmail.com>
2023-08-12 04:14:47 -0800

    New: Space by kennnyshiwa

---
+++ space/__init__.py
@@ -0,0 +1,8 @@
+from .space import Space
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+async def setup(bot):
+    await bot.add_cog(Space(bot))
