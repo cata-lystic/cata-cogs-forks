f26907fd20bba55793f055597f46dab29509e5eb
cata-lystic <ntqtmslf@protonmail.com>
2023-08-22 15:36:03 -0800

    .

--- extendedmodlog/eventmixin.py
+++ extendedmodlog/eventmixin.py
@@ -507,10 +507,9 @@ class EventMixin:
                 files = "\n".join(f"- {a.url}" for a in message.attachments)
                 clean_msg += f"\n"+files
 
-            hor_line = "\n----------------------------------------"
                 
             await channel.send(
-                f"{infomessage}\n```\n{clean_msg}```{hor_line}", allowed_mentions=self.allowed_mentions
+                f"{infomessage}\n```\n{clean_msg}```\n", allowed_mentions=self.allowed_mentions
             )
 
     @commands.Cog.listener()
