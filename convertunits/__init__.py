fe9b0b1c5c787596a12e5c5c0002646d07f596e8
cata-lystic <ntqtmslf@protonmail.com>
2023-08-23 21:28:49 -0800

    .

--- convertunits/__init__.py
+++ convertunits/__init__.py
@@ -1,5 +1,5 @@
 from redbot.core.bot import Red
-from .convertunits import Converterunits
+from .convertunits import Convertunits
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
