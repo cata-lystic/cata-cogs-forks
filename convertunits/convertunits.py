d49ac2a6809cd7a89390038ccf1c5369d5a47628
cata-lystic <ntqtmslf@protonmail.com>
2023-08-25 06:48:50 -0800

    .

--- convertunits/convertunits.py
+++ convertunits/convertunits.py
@@ -101,7 +101,7 @@ class Convertunits(commands.Cog):
                 msg += (", ".join(map(str, unit)))
 
 
-    @commands.command(aliases=['con', 'convertunits'], help=convHelpMenu(super().self))
+    @commands.command(aliases=['con', 'convertunits'], help=convHelpMenu)
     async def conv(self, ctx: commands.Context, convertFrom, convertTo, val: float=1):
         """Convert Units
         
