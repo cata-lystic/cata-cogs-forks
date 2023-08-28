87ebbd47ab03d632b43c31bfb561a50a21bfb5e4
cata-lystic <ntqtmslf@protonmail.com>
2023-08-27 22:06:37 -0800

    .

--- penis/penis.py
+++ penis/penis.py
@@ -80,7 +80,7 @@ class Penis(commands.Cog):
         Test"""
 
     @peniset.command(name='custom')
-    async def peni_custom(self, ctx, user: discord.Member, customMsg = None):
+    async def peni_custom(self, ctx, user: discord.Member, *, customMsg = None):
         """Custom Size/Message
 
         You can customize how large a certain user is, or give them a custom string as their size.
