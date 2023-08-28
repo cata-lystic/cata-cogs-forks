c2a6c783d1fae499fa6a71b9f2353923f998f0b9
cata-lystic <ntqtmslf@protonmail.com>
2023-08-28 02:54:43 -0800

    .

--- penis/penis.py
+++ penis/penis.py
@@ -84,7 +84,7 @@ class Penis(commands.Cog):
         for cust in customs:
             
             userID = cust
-            if ctx.bot.user.id == user.id:
+            if int(ctx.bot.user.id) == int(user.id):
                 continue # don't show bot on leaderboard
             userLength = customs[cust]
             member = guild.get_member(int(userID))
