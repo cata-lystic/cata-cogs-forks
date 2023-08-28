f9b4c5490ab75e1cc2fbedfcddc3f4ced51b47da
cata-lystic <ntqtmslf@protonmail.com>
2023-08-27 21:53:56 -0800

    .

--- penis/penis.py
+++ penis/penis.py
@@ -53,7 +53,7 @@ class Penis(commands.Cog):
             if ctx.bot.user.id == user.id:
                 dongs[user] = "8{}D".format("=" * 50)
             elif (userID in customs):
-                if (int(customs[userID])):
+                if isinstance(customs[userID], int):
                     length = customs[userID]
                     dongs[user] = "8{}D".format("=" * length)
                 else:
