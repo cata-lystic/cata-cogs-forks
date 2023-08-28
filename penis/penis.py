5999f91ac89bc5cb695152a269daf481090534c1
cata-lystic <ntqtmslf@protonmail.com>
2023-08-28 02:50:59 -0800

    .

--- penis/penis.py
+++ penis/penis.py
@@ -82,8 +82,12 @@ class Penis(commands.Cog):
         
         # remove from dict if isn't a number
         for cust in customs:
+
+                        # Bot always gets 50 length
             
             userID = cust
+            if ctx.bot.user.id == userID:
+                continue # don't show bot on leaderboard
             userLength = customs[cust]
             member = guild.get_member(int(userID))
 
